8dc273ba1527ab6b947f18c4e32d40e758eaefc1
chenmeng08 <chenmeng08@baidu.com>
2021-05-20 17:11:13 +0800

    add channel_test.go

--- src/study/channel/channel_test.go
+++ src/study/channel/channel_test.go
@@ -3,10 +3,30 @@ package main
 import (
 	"fmt"
 	"testing"
+	"time"
 )
 
 func Test_CP_ChapterInfo(t *testing.T) {
 	fmt.Println("test,test")
 	fmt.Println("222")
+}
+
+func Test_Select_Timeout(t *testing.T) {
+	c1 := make(chan string, 1)
+	go func() {
+		time.Sleep(time.Second * 1)
+		c1 <- "result 1"
+	}()
+	for {
+		select {
+		case res := <-c1:
+			fmt.Println(res)
+		case <-time.After(time.Second * 2):
+			fmt.Println("timeout 1")
+			return
+		}
+	}
+	fmt.Println("end............")
+
 }
 
